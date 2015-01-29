da6bd678424c91cb2a9e50bb975e5c63851ad3e1
Nemanja Mandic <nemanjans84@gmail.com>
2015-01-29 03:30:34 +0100

    IO example

---
+++ io-example.rb
@@ -0,0 +1,11 @@
+﻿File.open("./my_file.txt", "a+") do |file|
+ file.puts "Hello world!"
+end
+
+
+
+File.open("./my_file.txt", "a+") do |file|
+   file.puts "Hello world again !"
+ end  
+ 
+ puts IO.readlines('my_file.txt')
